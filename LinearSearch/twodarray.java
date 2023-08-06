c2f97ebce708ad5d3a48df466d986e5c42f98466
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-06 10:04:06 +0530

    2d

---
+++ LinearSearch/twodarray.java
@@ -0,0 +1,24 @@
+package LinearSearch;
+
+public class twodarray {
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12, 13 } };
+        for (int[] row : arr) {
+            System.out.println(java.util.Arrays.toString(row));
+        }
+        int target = 12;
+        System.out.println(Search(arr,target));
+        
+        
+    }
+    static int[] Search(int[][] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1}; // Element not found
+    }
+}

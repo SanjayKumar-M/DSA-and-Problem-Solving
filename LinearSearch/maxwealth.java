2294b953b1a0e68a46f6c83cbda5a19dee6e8eac
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-06 23:42:12 +0530

    finished

--- LinearSearch/maxwealth.java
+++ LinearSearch/maxwealth.java
@@ -1,8 +1,16 @@
 package LinearSearch;
 
+import java.util.Scanner;
 
 public class maxwealth {
    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int[] arr = new int[5];
+            for(int i=0;i<arr.length;i++){
+                System.out.println("Enter the element at index:"+i);
+                arr[i] = input.nextInt();
+            }
+            System.out.println(java.util.Arrays.toString(arr));
 
    }
 }

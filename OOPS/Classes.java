c55823279e5301017613cec0fbb5565e0c33b2e4
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-22 23:51:17 +0530

    updated

--- OOPS/Classes.java
+++ OOPS/Classes.java
@@ -2,8 +2,8 @@ package OOPS;
 
 public class Classes {
     public static void main(String[] args) {
-        Blockchain polygon = new Blockchain(2018,"Polygon","Sandeep Naiwal",167.23);
-        System.out.println(polygon);
+        Blockchain polygon = new Blockchain(2018,"Polygon","Sandeep Naiwal",167.2f);
+        System.out.print(polygon.founder,polygon.year,polygon.price,polygon.name);
     }
 
 }
@@ -13,4 +13,14 @@ class Blockchain {
     String name;
     String founder;
     float price;
+
+    //declaring constructor to input the values when an object is initialized
+
+    Blockchain(int year, String name,String founder, float price){
+        this.year = year;
+        this.name = name;
+        this.founder = founder;
+        this.price = price;
+
+    }
 }

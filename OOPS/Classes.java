9a9a09e79f8016b74b7ab41e94c974bfcfc77660
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-23 23:35:57 +0530

    constructor

--- OOPS/Classes.java
+++ OOPS/Classes.java
@@ -4,6 +4,8 @@ public class Classes {
     public static void main(String[] args) {
         Blockchain polygon = new Blockchain(2018,"Polygon","Sandeep Naiwal",167.2f);
         System.out.print(polygon.name);
+        Blockchain another = new Blockchain();
+        System.out.println(another.name);
     }
 
 }
@@ -13,6 +15,12 @@ class Blockchain {
     String name;
     String founder;
     float price;
+
+    Blockchain(){
+        //declaring constructor in another constructor or calling
+        this (2013,"Ethereum","Vitalik Buterin", 2000.34f);
+
+    }
 
     //declaring constructor to input the values when an object is initialized
 

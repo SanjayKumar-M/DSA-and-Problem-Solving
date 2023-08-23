b745cb2df7c4a323bbab3c9f7bced6424f100d43
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-23 23:48:24 +0530

    final

--- OOPS/Wrapper.java
+++ OOPS/Wrapper.java
@@ -2,9 +2,14 @@ package OOPS;
 
 public class Wrapper {
     public static void main(String[] args) {
-        //here the type is used to make the ref var as object to access its properties
-        Float num = 20;
-        System.out.println(Float.floatToIntBits(0));
+        // here the type is used to make the ref var as object to access its properties
+        Float num = 20.0f;
+
+        // final in java is a keyword used to make the variable constant, that is not to
+        // be changed in future. its like const in js
+        final String name = "Sanjay";
+
+        System.out.println(name);
     }
-    
+
 }

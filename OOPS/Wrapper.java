b669c6637c318a161f2d77c7a1341cb93bb904c0
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-08-23 23:41:01 +0530

    wrapper class

---
+++ OOPS/Wrapper.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        //here the type is used to make the ref var as object to access its properties
+        Float num = 20;
+        System.out.println(Float.floatToIntBits(0));
+    }
+    
+}

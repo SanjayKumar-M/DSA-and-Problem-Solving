a680d6afafd7056d24697c336a660224d277e79f
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-01-26 19:58:25 +0530

    revere the linkedlist

--- Striver/LinkedList/LinkedList.java
+++ Striver/LinkedList/LinkedList.java
@@ -11,80 +11,78 @@ class Node{
 }
 
 public class LinkedList{
-    Node head;
-    Node tail;
-    public LinkedList(){
-        this.head = null;
-        this.tail = null;
-
-    }
-
-    public void Insert(int data){
-        Node newNode = new Node(data);
-        if(head == null & tail == null){
-            head = tail = newNode;
-        }
-        else{
-            tail.next = newNode;
-            tail = newNode;
+        Node head;
+        Node tail;
+        public LinkedList(){
+            this.head = null;
+            this.tail = null;
         }
 
-
-    }
-    public void AtBeginning(int data){
-        Node beginningNode = new Node(data);
-        if(head == null && tail == null){
-            head = beginningNode;
-            tail = beginningNode;
-        }
-        else{
-           beginningNode.next = head;
-           head = beginningNode;
-        }
-    }
-    public void insertNodeAtSpecificPlace(int value, int place) {
-        Node specificNode = new Node(value);
-    
-        if (head == null && tail == null) {
-            head = tail = specificNode;
-        } else {
-            Node currentNode = head;
-            while (currentNode != null) {
-                if (currentNode.data == place) {
-                    specificNode.next = currentNode.next;
-                    currentNode.next = specificNode;
-                    break; // Break out of the loop once the insertion is done
-                }
-                currentNode = currentNode.next;
-            }
-        }
-    }
-    
-    public String Output(){
-        StringBuilder Result = new StringBuilder();
-        Node resultNode = head;
-        
-        while(resultNode != null){
-            Result.append(resultNode.data);
-            if(resultNode.next == null){
-                Result.append(" --> END");
+        public void Insert(int data){
+            Node newNode = new Node(data);
+            if(head == null && tail == null){
+                head = tail = newNode;
             }
             else{
-                Result.append(" --> ");
+                tail.next = newNode;
+                tail = newNode;
             }
-            resultNode= resultNode.next;
         }
-        return Result.toString();
-    }
+        public void AtBeginning(int data){
+            Node startingNode = new Node(data);
+            if(head == null && tail == null){
+                head = tail = startingNode;
+            }
+            else{
+                startingNode.next = head;
+                head = startingNode;
+            }
+        }
+        public void reverse(){
+            // 1->2->3->4->null  = 1-head, 4-tail,
+            // so 3 pointers will be there previous = null,current=current.next,next = next.next
 
+            Node previous = null;
+            Node current = head;
+            Node next = head.next;
+            while(next != null){
+                current.next = previous;
+                previous = current;
+                current = next;
+                next = next.next;
+            }
+            current.next = previous;
+
+            head = current;
+
+        }
+        
+        public String Output(){
+            StringBuilder result = new StringBuilder();
+            Node currentNode = head;
+            while(currentNode != null){
+                result.append(currentNode.data);
+                if(currentNode.next == null){
+                    result.append(" --> END");
+                }
+                else{
+                    result.append(" --> ");
+                }
+
+                currentNode = currentNode.next;
+
+            } 
+            return result.toString();
+        }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.Insert(1);
-        ll.Insert(1);
-        ll.Insert(1);
-        ll.Insert(1);
+        ll.Insert(2);
+        ll.Insert(3);
+        ll.Insert(4);
         ll.AtBeginning(10);
-        ll.insertNodeAtSpecificPlace(20, 1);
+        ll.reverse();
+        // ll.insertNodeAtSpecificPlace(20, 1);
         System.out.println(ll.Output());
     }
 }

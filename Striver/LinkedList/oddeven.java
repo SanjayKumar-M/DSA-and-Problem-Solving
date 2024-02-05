156c8559694bfba4484543e851817cc81ce26900
SanjayKumar-M <sanjaymoad786@gmail.com>
2024-02-05 22:33:50 +0530

    odd even

--- Striver/LinkedList/oddeven.java
+++ Striver/LinkedList/oddeven.java
@@ -1,3 +1,35 @@
-public class oddeven {
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+         if (head == null || head.next == null) {
+        // There are fewer than two nodes, no rearrangement needed
+        return head;
+    }
+
+    ListNode odd = head;
+    ListNode even = head.next;
+    ListNode evenStart = even; // Remember the start of the even-indexed nodes
+
+    while (even != null && even.next != null) {
+        odd.next = even.next; // Connect odd node to the next odd node
+        odd = odd.next; // Move to the next odd node
+
+        even.next = odd.next; // Connect even node to the next even node
+        even = even.next; // Move to the next even node
+    }
+
+    // At this point, odd points to the last odd node, so connect it to the start of even-indexed nodes
+    odd.next = evenStart;
+    return head;
+        
+    }
 }
--- Striver/LinkedList/reverse.java
+++ Striver/LinkedList/reverse.java
@@ -1,3 +1,37 @@
-public class reverse {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       if(head == null || head.next == null) return head;
+       ListNode revNode = reverseList(head.next);
     
+    // Reverse the pointers
+    head.next.next = head; // Make the next node of the next node point back to the current node
+    head.next = null; // Make the current node point to null
+    
+    // Return the head of the reversed list
+    return revNode;
+       
+    //    ListNode previous = null;
+    //    ListNode current = head;
+    //    ListNode nextNode = head.next;
+    //    while(nextNode != null){
+    //        current.next = previous;
+    //        previous = current;
+    //        current = nextNode;
+    //        nextNode = nextNode.next;
+    //    }
+    //    current.next = previous;
+       
+    //    return current;
+        
+    }
 }
